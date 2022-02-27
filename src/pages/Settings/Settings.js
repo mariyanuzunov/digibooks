7bbdbff930657707571612c3ee5fa196fb138e4e
Mariyan Uzunov <themariyanuzunov@gmail.com>
2022-02-27 10:44:33 +0200

    settings page

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -1,9 +1,58 @@
+import { Link } from "react-router-dom";
 import Header from "../../components/Header/Header";
+
+import styles from "./Settings.module.css";
+
+const GENERAL_SETTINGS = [
+  "NOTIFICATIONS AND EMAILS",
+  "USER MANAGEMENT",
+  "PHYSICAL LIBRARIES",
+  "READING EVENTS",
+  "INVOICING",
+  "BOOK STATISTICS",
+  "READERS STATISTICS",
+  "EVENTS STATISTICS",
+];
+
+const BOOK_SETTINGS = [
+  "MANAGE GENRES",
+  "BOOK VISIBILITY",
+  "AUTHORS DATABASE",
+  "BOOK COVERS",
+];
 
 export default function Settings() {
   return (
     <>
       <Header />
+      <section className={styles.container}>
+        <section className={styles.settings}>
+          <header className={styles.settingsHeader}>
+            <h3 className={styles.settingsTitle}>General Settings</h3>
+          </header>
+          <OptionsList options={GENERAL_SETTINGS} />
+        </section>
+        <section className={styles.settings}>
+          <header className={styles.settingsHeader}>
+            <h3>Book Settings</h3>
+            <button className={styles.btnAdd}>Add New</button>
+          </header>
+          <OptionsList options={BOOK_SETTINGS} />
+        </section>
+      </section>
+    </>
+  );
+}
+
+function OptionsList({ options }) {
+  return (
+    <>
+      {options.map((x) => (
+        <p key={x} className={styles.settingsOption}>
+          <Link to="">{x}</Link>
+          <span className={styles.line}></span>
+        </p>
+      ))}
     </>
   );
 }
